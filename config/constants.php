a179cbec595e0b640019d93da7907525f510ab67
Jesus_Enriquez <jenriquez0@ucol.mx>
2024-05-20 19:15:54 -0600

    Imagen del login

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define('ROOT_URL', 'http://localhost/project333/');
+define('ROOT_URL', 'http://localhost/project7/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
